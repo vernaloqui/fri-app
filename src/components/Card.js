42adae2ba0bd08fc18cc2ff21f72d426fb3682a5
vernaloqui <vernadetteloquinario@gmail.com>
2022-12-03 11:03:20 +0800

    card style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import {Link} from 'react-router-dom'
 function Card(prop){
     return (
         <div>
-            <div className="card" style={{width: "18rem", margin:"2px"}}>
+            <div className="card col-12 col-lg-3 col-md-6" style={{width: "18rem", margin:"2px"}}>
                 <img src={prop.img} className="card-img-top" alt="text"/>
-                <div className="card-body">
+                <div className="card-body d-flex flex-column">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <Link to="/" className="btn btn-primary">Go Somewhere</Link>
